0c715d1a6ae1a078dc3b8a3f0a5cdc1c1fdd1a44
Hackeristi <klaudia.n.klim@gmail.com>
2025-12-28 08:57:21 +0100

    Unit Tests

---
+++ app/src/test/java/pl/pb/optigai/AnalysisViewModelTest.kt
@@ -0,0 +1,44 @@
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+import android.net.Uri
+import pl.pb.optigai.utils.data.AnalysisViewModel
+import pl.pb.optigai.utils.data.DetectionResult
+class AnalysisViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun initPhotoUri_setsUriOnlyOnce() {
+        val viewModel = AnalysisViewModel()
+        val uri = mock(Uri::class.java)
+
+        viewModel.initPhotoUri(uri)
+        viewModel.initPhotoUri(mock(Uri::class.java))
+
+        assertEquals(uri, viewModel.photoUri.value)
+    }
+
+    @Test
+    fun setDetectionResult_updatesLiveData() {
+        val viewModel = AnalysisViewModel()
+        val list = emptyList<DetectionResult>()
+
+        viewModel.setDetectionResult(list)
+
+        assertEquals(list, viewModel.analysisDetectionResults.value)
+    }
+
+    @Test
+    fun setSummaryTextResult_updatesLiveData() {
+        val viewModel = AnalysisViewModel()
+        val text = "Test summary"
+
+        viewModel.setSummaryTextResult(text)
+
+        assertEquals(text, viewModel.analysisSummaryTextResult.value)
+    }
+}
